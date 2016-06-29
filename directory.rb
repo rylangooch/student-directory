633ea8b3138c4e473571e80ebc45502c8326a6d9
Rylan <rylangooch@gmail.com>
2016-06-29 23:38:57 +0000

    Added the option to print individual cohorts

--- directory.rb
+++ directory.rb
@@ -1,11 +1,16 @@
 def input_students
-    puts "Please enter the name and of the first student"
+    puts "Please enter the name of the first student"
     # create an empty array
     students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
+        puts "Please enter the student's cohort"
+        cohort = gets.chomp
+        if cohort.empty? 
+            cohort = "undisclosed"
+        end
         puts "Please enter the student's hobbies"
         hobbies = gets.chomp
         puts "Please enter the student's country of birth"
@@ -13,7 +18,7 @@ def input_students
         puts "Please enter the students height in cm"
         height = gets.chomp
         # add the student hash to the array
-        students << {name: name, cohort: :november, hobbies: hobbies, cob: cob, height: height}
+        students << {name: name, cohort: cohort.intern, hobbies: hobbies, cob: cob, height: height}
         puts "Now we have #{students.count} students. Please enter the name of the next student or press return to quit"
         # get another name from the user
         name = gets.chomp
@@ -27,7 +32,7 @@ def print_header
     puts "___________"
 end
 
-def print(students)
+def print_s(students)
     counter = 0
     while counter < students.length
         puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
@@ -38,12 +43,26 @@ def print(students)
     end
 end
 
+def print_by_cohort(students)
+    cohort_month = []
+    puts "Enter the cohort you wish to view"
+    month = gets.chomp
+    students.map do |student|
+        if student[:cohort] == month.to_sym
+            cohort_month << student
+        end
+    end
+    print_s(cohort_month)
+end
+
 
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
+puts students
 print_header
-print(students)
+print_s(students)
 print_footer(students)
+print_by_cohort(students)
