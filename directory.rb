de92591f583b07e0fec495f01a79e36f48c47658
Rylan <rylangooch@gmail.com>
2016-06-29 18:18:43 +0000

    Added additional details to directory (hobbies, country of birth and height)

--- directory.rb
+++ directory.rb
@@ -1,15 +1,20 @@
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
+    puts "Please enter the name and of the first student"
     # create an empty array
     students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
+        puts "Please enter the student's hobbies"
+        hobbies = gets.chomp
+        puts "Please enter the student's country of birth"
+        cob = gets.chomp
+        puts "Please enter the students height in cm"
+        height = gets.chomp
         # add the student hash to the array
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        students << {name: name, cohort: :november, hobbies: hobbies, cob: cob, height: height}
+        puts "Now we have #{students.count} students. Please enter the name of the next student or press return to quit"
         # get another name from the user
         name = gets.chomp
     end 
@@ -26,6 +31,9 @@ def print(students)
     counter = 0
     while counter < students.length
         puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+        puts "Hobbies: #{students[counter][:hobbies]}"
+        puts "Country of birth: #{students[counter][:cob]}"
+        puts "Height: #{students[counter][:height]}"
         counter += 1
     end
 end
